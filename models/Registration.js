8a713dce754ad1787330ab3648dc7e839bb027c3
chouhanpawan95-ui <chouhanpawan95@gmail.com>
2025-10-23 15:16:02 +0530

    pawan-api changes

--- models/Registration.js
+++ models/Registration.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const registrationSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  owner:{ type: String },
+  owner:{ type: String, required: true },
   location: { type: String, required: true },
   country: { type: String, required: true },
   state: { type: String, required: true },
